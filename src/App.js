46e48c0b5ddb46937159274508c8bd70a5816201
Abraham Ramos <Abraham.Ramos@toshibagcs.com>
2019-06-29 14:21:26 -0500

    first commit saturday 29

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// material ui
+import Typography from "@material-ui/core/Typography";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Typography align ="center" variant="h2" gutterBottom>
+        Mi gifcp
+      </Typography>
+    </React.Fragment>
   );
 }
 
